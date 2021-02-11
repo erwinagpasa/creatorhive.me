bc0f7c1376c6e6ffcee240a4ca6a25594fae9ec4
erwinagpasa <istran.net@gmail.com>
2021-02-11 10:53:10 +0300

    adding home sections

---
+++ components/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Creatorhive</title>
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/global.css?v=1.2">
+</head>
+
+<body>
